1a1b9405893f3566658561bd0dd031f73103021d
valerium-dev <admin@valerium.dev>
2020-10-04 19:52:50 -0500

    Finished UI for Books, Authors, and Publishers page

--- app.py
+++ app.py
@@ -2,39 +2,46 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def splash():
     return render_template('splash.html')
+
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
+
 # TODO: Phase I - Fetch book data from APIs
 #       Phase II - Fetch book data from DB
 @app.route('/books/', methods=['GET'])
 def books():
-    books = ['Beloved by Tony Morrison', 'The Magicians by Lev Grossman']
+    books = ['Beloved by Tony Morrison', 'The Magicians by Lev Grossman', 'Fluent Python by Luciano Ramalho']
     return render_template('books.html', books=books)
+
 
 # TODO: Phase I - Fetch author data from APIs
 #       Phase II - Fetch author data from DB
 @app.route('/authors/', methods=['GET'])
 def authors():
-    authors = ['Author Instance 1', 'Author Instance 2']
+    authors = ['Tony Morrison', 'Lev Grossman', 'Luciano Ramalho']
     return render_template('authors.html', authors=authors)
+
 
 # TODO: Phase I - Fetch pub data from APIs
 #       Phase II - Fetch pub data from DB
 @app.route('/publishers/', methods=['GET'])
 def publishers():
-    pubs = ['Publisher Instance 1', 'Publisher Instance 2']
-    return render_template('publishers.html', pubs=pubs)
+    publishers = ['Alfred A. Knopf', '', '']
+    return render_template('publishers.html', publishers=publishers)
+
 
 # TODO: Back-end work. Implement search algo
 @app.route('/search/', methods=['GET', 'POST'])
 def search():
     return "Search Results"
+
 
 if __name__ == '__main__':
     app.run(debug=True)

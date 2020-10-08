1ae4594513ba3f54ce3ef32379ae1feb96e47c5e
valerium-dev <admin@valerium.dev>
2020-10-08 15:24:27 -0500

    Added routes for model instances based on instance name

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ def books():
     return render_template('books.html', books=books)
 
 
+# TODO: Change route to use book id instead of book name in P.II
+@app.route('/books/<string:book_name>')
+def book_instance(book_name):
+    book = dict()
+    if book_name == 'beloved':
+        book['title'] = ' '.join(book_name.split('-')).title()
+    elif book_name == 'the-magicians':
+        book['title'] = ' '.join(book_name.split('-')).title()
+    else:
+        book['title'] = ' '.join(book_name.split('-')).title()
+
+    return render_template('book-instance.html', book=book)
+
+
 # TODO: Phase I - Fetch author data from APIs
 #       Phase II - Fetch author data from DB
 @app.route('/authors/', methods=['GET'])
@@ -29,12 +43,40 @@ def authors():
     return render_template('authors.html', authors=authors)
 
 
+# TODO: Change route to use author id instead of author name in P.II
+@app.route('/authors/<string:author_name>')
+def author_instance(author_name):
+    author = dict()
+    if author == 'toni-morrison':
+        author['name'] = ' '.join(author_name.split('-')).title()
+    elif author_name == 'lev-grossman':
+        author['name'] = ' '.join(author_name.split('-')).title()
+    else:
+        author['name'] = ' '.join(author_name.split('-')).title()
+
+    return render_template('author-instance.html', author=author)
+
+
 # TODO: Phase I - Fetch pub data from APIs
 #       Phase II - Fetch pub data from DB
 @app.route('/publishers/', methods=['GET'])
 def publishers():
     publishers = ['Alfred A. Knopf', 'Viking Press', "O'Reilly"]
     return render_template('publishers.html', publishers=publishers)
+
+
+# TODO: Change route to use publisher id instead of publisher name in P.II
+@app.route('/publishers/<string:pub_name>')
+def publisher_instance(pub_name):
+    pub = dict()
+    if pub_name == 'alfred-a-knopf':
+        pub['name'] = ' '.join(pub_name.split('-')).title()
+    elif pub_name == 'viking-press':
+        pub['name'] = ' '.join(pub_name.split('-')).title()
+    else:
+        pub['name'] = ' '.join(pub_name.split('-')).title()
+
+    return render_template('publisher-instance.html', publisher=pub)
 
 
 # TODO: Back-end work. Implement search algo

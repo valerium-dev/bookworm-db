76a9e01d18c60f4de612d0ae8d2580760b42e171
valerium-dev <admin@valerium.dev>
2020-10-20 19:07:45 -0500

    Removed hardcoded data on books and book instances. Implemented connection to database

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+atlas_user = os.getenv("ATLAS_USER")
+atlas_pwd = os.getenv("ATLAS_PASSWD")
+mongo = MongoClient(f'mongodb+srv://{atlas_user}:{atlas_pwd}@cluster0.rh1w0.mongodb.net/book_worm_database?retryWrites=true&w=majority')
+books_collection = mongo.book_worm_database.Books
+authors_collection = mongo.book_worm_database.Authors
+publishers_collection = mongo.book_worm_database.Publishers
 
 @app.route('/')
 def splash():
@@ -17,51 +25,22 @@ def about():
 #       Phase II - Fetch book data from DB
 @app.route('/books/', methods=['GET'])
 def books():
-    books = ['Beloved by Tony Morrison', 'The Magicians by Lev Grossman', '1984 by George Orwell']
-    return render_template('books.html', books=books)
+    book_list = []
+    for book in books_collection.find():
+        temp = dict()
+        temp['_id'] = book['_id']
+        temp['title'] = book['title']
+        temp['authors'] = book['authors']
+        temp['genre'] = book['genre']
+        temp['thumbnail_url'] = book['thumbnail_url']
+        book_list.append(temp)
+
+    return render_template('books.html', books=book_list)
 
 
 # TODO: Change route to use book id instead of book name in P.II
 @app.route('/books/<string:book_name>')
 def book_instance(book_name):
-    book = dict()
-    if book_name == 'beloved':
-        book['title'] = ' '.join(book_name.split('-')).title()
-        book['author'] = "Toni Morrison"
-        book['page_count']="352"
-        book['maturity']="NOT_MATURE"
-        book['lang']= "en"
-        book['publisher'] = "Alfred A. Knopf"
-        book['published_date'] = "2019"
-        book['description'] = "Upon the original publication of Beloved, John Leonard wrote in the Los Angeles Times: \"I can't imagine American literature without it.\" Nearly two decades later, The New York Times chose Beloved as the best American novel of the previous fifty years. Toni Morrison's magnificent Pulitzer Prize-winning work--first published in 1987--brought the wrenching experience of slavery into the literature of our time, enlarging our comprehension of America's original sin. Set in post-Civil War Ohio, it is the story of Sethe, an escaped slave who has lost a husband and buried a child; who has withstood savagery and not gone mad. Sethe, who now lives in a small house on the edge of town with her daughter, Denver, her mother-in-law, Baby Suggs, and a disturbing, mesmerizing apparition who calls herself Beloved. Sethe works at \"beating back the past,\" but it makes itself heard and felt incessantly: in her memory; in Denver's fear of the world outside the house; in the sadness that consumes Baby Suggs; in the arrival of Paul D, a fellow former slave; and, most powerfully, in Beloved, whose childhood belongs to the hideous logic of slavery and who has now come from the \"place over there\" to claim retribution for what she lost and for what was taken from her. Sethe's struggle to keep Beloved from gaining possession of the present--and to throw off the long-dark legacy of the past--is at the center of this spellbinding novel. But it also moves beyond its particulars, combining imagination and the vision of legend with the unassailable truths of history."
-        book['isbn13'] = "9780525659273"
-        book['genre'] = "Fiction"
-        book['thumbnail_url'] = "http://books.google.com/books/content?id=5xG_DwAAQBAJ&printsec=frontcover&img=1&source=gbs_api"
-    elif book_name == 'the-magicians':
-        book['title'] = ' '.join(book_name.split('-')).title()
-        book['author'] = "Lev Grossman"
-        book['maturity'] = "NOT_MATURE"
-        book['page_count'] = "416"
-        book['lang'] = "en"
-        book['publisher'] = "Penguin"
-        book['published_date'] = "2009-08-11"
-        book['description'] = "The New York Times bestselling novel about a young man practicing magic in the real world, now an original series on SYFY “The Magicians is to Harry Potter as a shot of Irish whiskey is to a glass of weak tea. . . . Hogwarts was never like this.” —George R.R. Martin “Sad, hilarious, beautiful, and essential to anyone who cares about modern fantasy.” —Joe Hill “A very knowing and wonderful take on the wizard school genre.” —John Green “The Magicians may just be the most subversive, gripping and enchanting fantasy novel I’ve read this century.” —Cory Doctorow “This gripping novel draws on the conventions of contemporary and classic fantasy novels in order to upend them . . . an unexpectedly moving coming-of-age story.” —The New Yorker “The best urban fantasy in years.” —A.V. Club Quentin Coldwater is brilliant but miserable. A high school math genius, he’s secretly fascinated with a series of children’s fantasy novels set in a magical land called Fillory, and real life is disappointing by comparison. When Quentin is unexpectedly admitted to an elite, secret college of magic, it looks like his wildest dreams have come true. But his newfound powers lead him down a rabbit hole of hedonism and disillusionment, and ultimately to the dark secret behind the story of Fillory. The land of his childhood fantasies turns out to be much darker and more dangerous than he ever could have imagined. . . . The prequel to the New York Times bestselling book The Magician King and the #1 bestseller The Magician's Land, The Magicians is one of the most daring and inventive works of literary fantasy in years. No one who has escaped into the worlds of Narnia and Harry Potter should miss this breathtaking return to the landscape of the imagination."
-        book['isbn13'] = "9781101082287"
-        book['genre'] = "Fiction"
-        book['thumbnail_url'] = "http://books.google.com/books/content?id=d84AspgFjSwC&printsec=frontcover&img=1&source=gbs_api"
-
-    else:
-        book['title'] = '1984'.join(book_name.split('-')).title()
-        book['author'] = "George Orwell"
-        book['page_count'] = "648"
-        book['maturity'] = "NOT_MATURE"
-        book['lang'] = "en"
-        book['publisher'] = "Houghton Mifflin Harcourt"
-        book['published_date'] = "1983-10-17"
-        book['description'] = "George Orwell’s 1984 takes on new life with extraordinary relevance and renewed popularity. “Orwell saw, to his credit, that the act of falsifying reality is only secondarily a way of changing perceptions. It is, above all, a way of asserting power.”—The New Yorker In 1984, London is a grim city in the totalitarian state of Oceania where Big Brother is always watching you and the Thought Police can practically read your mind. Winston Smith is a man in grave danger for the simple reason that his memory still functions. Drawn into a forbidden love affair, Winston finds the courage to join a secret revolutionary organization called The Brotherhood, dedicated to the destruction of the Party. Together with his beloved Julia, he hazards his life in a deadly match against the powers that be. Lionel Trilling said of Orwell’s masterpiece, “1984 is a profound, terrifying, and wholly fascinating book. It is a fantasy of the political future, and like any such fantasy, serves its author as a magnifying device for an examination of the present.” Though the year 1984 now exists in the past, Orwell’s novel remains an urgent call for the individual willing to speak truth to power."
-        book['isbn13'] = "9780547249643"
-        book['genre'] = "Fiction"
-        book['thumbnail_url'] = "http://books.google.com/books/content?id=kotPYEqx7kMC&printsec=frontcover&img=1&source=gbs_api"
 
     return render_template('book-instance.html', book=book)
 
